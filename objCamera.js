06b8992f9a58dd0f7b034ea8f00d977de9b3d3cd
VenoDev <47304933+VenoDev@users.noreply.github.com>
2020-04-05 20:32:29 +0200

    Update objCamera.js

--- objCamera.js
+++ objCamera.js
@@ -2,6 +2,10 @@ function Camera() {
     this.y = 0;
     this.x = 0;
     this.z = 0;
+    
+    this.rotX = 0;
+    this.rotY = 0;
+    this.rotZ = 0;
 
     this.show = function() {
         ellipse(40);
