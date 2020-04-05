1739d043dde343674c5060351e1cbd3520ce8cc2
VenoDev <47304933+VenoDev@users.noreply.github.com>
2020-04-05 20:54:28 +0200

    test

--- sketch.js
+++ sketch.js
@@ -6,9 +6,11 @@ let objects = [
 let objectTypes = ["point light", "camera", "cube"];
 
 function setup() { //Called on the start of the program
-    
+    smooth();
+    createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() { //Called every frame
-
+    vertex(-100, 0, 0)
+    vertex(100, 0, 0)
 }
