4b0b3e1642711c5f11e8623066b147b5df244bd7
VenoDev <47304933+VenoDev@users.noreply.github.com>
2020-04-05 21:33:40 +0200

    rewrite

--- sketch.js
+++ sketch.js
@@ -5,18 +5,38 @@ let objects = [
 
 let objectTypes = ["point light", "camera", "cube"];
 
+let lastX = 0;
+
 function setup() { //Called on the start of the program
     smooth();
     createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() { //Called every frame
-    strokeWeight(3);
-    stroke(237, 34, 93);
-    beginShape(LINES);
-    vertex(-100, 0, 0);
-    vertex(100, 0, 0);
+    background(21, 21, 21);
+    //rotateX(frameCount * 0.01);
+    //rotateZ(frameCount * 0.01);
+    rotateX(-QUARTER_PI);
+    rotateY(lastX / 100 + QUARTER_PI);
+	strokeWeight(1);
+	stroke(0, 255, 0);
+	beginShape(LINES);
+    vertex(-300, 0, 0);
+    vertex(300, 0, 0);
     endShape();
-    rotateX(frameCount * 0.01);
-    rotateZ(frameCount * 0.01);
+    stroke(255, 0, 0);
+    beginShape(LINES);
+    vertex(0, 0, 300);
+    vertex(0, 0, -300);
+    endShape();
+    stroke(0, 0, 255)
+    beginShape(LINES);
+    vertex(0, 300, 0);
+    vertex(00, -300, 0);
+    endShape();
+}
+
+function mouseDragged() {
+    lastX = lastX + movedX * 2;  //set new last X
+    return false;
 }
