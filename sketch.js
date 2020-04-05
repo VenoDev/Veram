77942488c5977f94cb7faa017b806351603bd321
VenoDev <47304933+VenoDev@users.noreply.github.com>
2020-04-05 20:18:57 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let objects = [
+    "camera",
+    "baseplate"
+];
+
+let objectTypes = ["point light", "camera", "cube"];
+
+function setup() { //Called on the start of the program
+    
+}
+
+function draw() { //Called every frame
+
+}
